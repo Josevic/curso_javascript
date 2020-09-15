ed16d817d814bc9ae87fa556770cfe21f8c088f9
jose <josetorres.asir@gmail.com>
2020-09-15 17:34:59 +0200

    borramos codigo creamos fat arrow function en main.js

--- main.js
+++ main.js
@@ -1,14 +1,2 @@
-let num1 = 20;
-let num2 = 3;
-name = "Chelsey";
-
-function contador() {
-  let cont = 0;
-  if ((name = "Chelsey")) {
-    cont = cont + 1;
-  } else {
-    cont = cont + 0;
-  }
-  return name + " anotó otro " + num1 * num2 + " y un: " + cont;
-}
-console.log(contador());
+const sumar = (a, b) => a + b;
+console.log(sumar(2, 5));
