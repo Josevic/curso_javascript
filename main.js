2dd4070367ff30289869e98a9df40ddbf0d99467
jose <josetorres.asir@gmail.com>
2020-09-15 16:55:21 +0200

    Inicio de proyecto codigo javascript

---
+++ main.js
@@ -0,0 +1,19 @@
+let valor = 1;
+console.log("HOla mundo", valor);
+
+let varlor = 0;
+console.log("HOla variable: ", varlor);
+total = valor + varlor;
+console.log(total);
+
+let suma = total;
+console.log(suma);
+suma = total + valor;
+console.log(suma);
+
+valor2 = 0;
+valor2 = "A";
+valor2 = valor2;
+console.log(valor2);
+valor2 = valor2 + 0;
+console.log(valor2);
