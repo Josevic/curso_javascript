7599ffa70cbf6ccfb0517dd4e2b8497e90a53483
jose <josetorres.asir@gmail.com>
2020-09-15 17:02:30 +0200

    Creada una función borrado codigo anterior

--- main.js
+++ main.js
@@ -1,19 +1,10 @@
-let valor = 1;
-console.log("HOla mundo", valor);
-
-let varlor = 0;
-console.log("HOla variable: ", varlor);
-total = valor + varlor;
-console.log(total);
-
-let suma = total;
-console.log(suma);
-suma = total + valor;
-console.log(suma);
-
-valor2 = 0;
-valor2 = "A";
-valor2 = valor2;
-console.log(valor2);
-valor2 = valor2 + 0;
-console.log(valor2);
+let num1 = 20;
+let num2 = 3;
+name = "Chelsey";
+function multiplicar() {
+  return num1 * num2;
+}
+multiplicar();
+console.log(multiplicar());
+multiplicacion = multiplicar();
+console.log(multiplicacion);
