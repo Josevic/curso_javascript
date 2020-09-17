8ed0f6fca4c186917d98915674a413e1add53a1b
jose <josetorres.asir@gmail.com>
2020-09-17 16:34:03 +0200

    Pasando mas propiedades del objeto a traves de la función

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const servicios = {
-  api: {},
+  api: "soy api",
   mailer: "soy mailer",
   apollo: {},
 };
 const enviarCorreo = (srv) => {
-  const { mailer } = srv;
-  console.log(mailer);
+  const { mailer, api } = srv;
+
+  console.log(mailer, "  ", api);
 };
 enviarCorreo(servicios);
 //Perimite consultar los objetos a través de una función
